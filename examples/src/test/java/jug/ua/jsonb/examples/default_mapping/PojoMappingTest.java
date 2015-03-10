706b1ecc85e363a2798ed64830da526c1b999991
Olena_Syrota <sirotae@gmail.com>
2015-03-10 23:34:20 +0200

    example of one-to-one assosiation of type "composition" added

--- examples/src/test/java/jug/ua/jsonb/examples/default_mapping/PojoMappingTest.java
+++ examples/src/test/java/jug/ua/jsonb/examples/default_mapping/PojoMappingTest.java
@@ -46,25 +46,48 @@ public class PojoMappingTest {
 
     @Test
     public void inheritanceFromJsonTest() {
-        Dog dogExp = new Dog();
-        dogExp.setAge(1);
-        dogExp.setName("Dog");
+        SubClass exp = new SubClass();
+        exp.setSuperclassField1(1);
+        exp.setSubclassField1("Dog");
 
-        Dog dogAct = jsonb.fromJson("{\"age\":1, \"name\":\"Dog\"}", Dog.class);
-        assertEquals(dogExp.getAge(),dogAct.getAge());
-        assertEquals(dogExp.getName(),dogAct.getName());
+        SubClass act = jsonb.fromJson("{\"superclassField1\":1, \"subclassField1\":\"Dog\"}", SubClass.class);
+        assertEquals(exp.getSubclassField1(),act.getSubclassField1());
+        assertEquals(exp.getSuperclassField1(),act.getSuperclassField1());
     }
 
     @Test
     public void inheritanceToJsonTest() {
-        Dog pojo = new Dog();
-        pojo.setAge(1);
-        pojo.setName("Dog");
+        SubClass pojo = new SubClass();
+        pojo.setSuperclassField1(1);
+        pojo.setSubclassField1("Dog");
 
         String act = jsonb.toJson(pojo);
-        assertEquals("{\"age\":1,\"name\":\"Dog\"}", act);
+        assertEquals("{\"subclassField1\":\"Dog\",\"superclassField1\":1}", act);
     }
 
+    public void compositeFromJsonTest() {
+        ClassWithOneToOneComposite pojo = new ClassWithOneToOneComposite();
+        Composited composited = new Composited();
+        composited.setCompositedField1("c1");
+        composited.setCompositedField2("c2");
+        pojo.setField1(1);
+        pojo.setComposited(composited);
+
+        POJO pojoAct = jsonb.fromJson("{\"field1\":1, \"composited\": {\"compositedField1\":\"c1\", \"compositedField2\":\"c2\"}}", POJO.class);
+        assertEquals(pojo,pojoAct);
+    }
+
+    public void compositeToJsonTest() {
+        ClassWithOneToOneComposite pojo = new ClassWithOneToOneComposite();
+        Composited composited = new Composited();
+        composited.setCompositedField1("c1");
+        composited.setCompositedField2("c2");
+        pojo.setField1(1);
+        pojo.setComposited(composited);
+
+        String act = jsonb.toJson(pojo);
+        assertEquals("{\"field1\":1, \"composited\": {\"compositedField1\":\"c1\", \"compositedField2\":\"c2\"}}", act);
+    }
 
 
     private static class POJO {
@@ -107,37 +130,107 @@ public class PojoMappingTest {
     }
 
 
-    public static class Animal {
-        private int age;
+    public static class SuperClass {
+        private int superclassField1;
 
-        public Animal() {
+        public SuperClass() {
         }
 
-        public int getAge() {
-            return age;
+        public int getSuperclassField1() {
+            return superclassField1;
         }
 
-        public void setAge(int age) {
-            this.age = age;
+        public void setSuperclassField1(int superclassField1) {
+            this.superclassField1 = superclassField1;
         }
+
     }
 
-    public static class Dog extends Animal {
-        private String name;
+    public static class SubClass extends SuperClass {
+        private String subclassField1;
 
-        public Dog() {
+        public SubClass() {
             super();
         }
 
-        public String getName() {
-            return name;
+        public String getSubclassField1() {
+            return subclassField1;
         }
 
-        public void setName(String name) {
-            this.name = name;
+        public void setSubclassField1(String subclassField1) {
+            this.subclassField1 = subclassField1;
         }
-
-
     }
 
+
+    public static class ClassWithOneToOneComposite {
+        private int field1;
+        private Composited composited;
+
+        public int getField1() {
+            return field1;
+        }
+
+        public void setField1(int field1) {
+            this.field1 = field1;
+        }
+
+        public Composited getComposited() {
+            return composited;
+        }
+
+        public void setComposited(Composited composited) {
+            this.composited = composited;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            ClassWithOneToOneComposite that = (ClassWithOneToOneComposite) o;
+
+            if (field1 != that.field1) return false;
+            if (composited != null ? !composited.equals(that.composited) : that.composited != null) return false;
+
+            return true;
+        }
+    }
+
+    public class Composited {
+        private String compositedField1;
+        private String compositedField2;
+
+        public String getCompositedField1() {
+            return compositedField1;
+        }
+
+        public void setCompositedField1(String compositedField1) {
+            this.compositedField1 = compositedField1;
+        }
+
+        public String getCompositedField2() {
+            return compositedField2;
+        }
+
+        public void setCompositedField2(String compositedField2) {
+            this.compositedField2 = compositedField2;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Composited that = (Composited) o;
+
+            if (compositedField1 != null ? !compositedField1.equals(that.compositedField1) : that.compositedField1 != null)
+                return false;
+            if (compositedField2 != null ? !compositedField2.equals(that.compositedField2) : that.compositedField2 != null)
+                return false;
+
+            return true;
+        }
+
+    }
 }
