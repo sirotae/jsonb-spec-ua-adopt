36f225dec2e50996da276aec5d8dfae56f4a151e
Olena_Syrota <sirotae@gmail.com>
2015-04-21 20:10:47 +0300

    adding ser/deser int adding deser boolean

--- examples/src/test/java/jug/ua/meetup/Case4Primitives.java
+++ examples/src/test/java/jug/ua/meetup/Case4Primitives.java
@@ -1,5 +1,6 @@
 package jug.ua.meetup;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,10 +17,11 @@ import static org.junit.Assert.assertEquals;
  * Created by Olena_Syrota on 4/19/2015.
  *
  * scenario:
- *   deserializing null to primitive
- *   - jackson can fail on nulls
+ *   - ser/deser value
+ *   - deserializing null to primitive
+ *      - jackson can fail on nulls
  */
-public class Case4PrimitiveNulls {
+public class Case4Primitives {
 
     //GSON
     private Gson gson = new Gson();
@@ -31,6 +33,52 @@ public class Case4PrimitiveNulls {
     //GENSON
     private Genson genson = new Genson();
 
+
+    @Test
+    public void gsonSerializeIntPrimitive() throws IOException {
+        int val = 1;
+        String exp = "1";
+
+        String gsonRes = gson.toJson(val);
+        assertEquals(exp, gsonRes);
+
+        String jacksonRes = jackson.writeValueAsString(val);
+        assertEquals(exp, jacksonRes);
+
+        String gensonRes = genson.serialize(val);
+        assertEquals(exp, gensonRes);
+    }
+
+    @Test
+    public void gsonDeserializeIntPrimitive() throws IOException {
+        String val = "1";
+        int exp = 1;
+
+        int gsonRes = gson.fromJson(val, int.class);
+        assertEquals(exp, gsonRes);
+
+        int jacksonRes = jackson.readValue(val, int.class);
+        assertEquals(exp, jacksonRes);
+
+        int gensonRes = genson.deserialize(val, int.class);
+        assertEquals(exp, gensonRes);
+    }
+
+
+    @Test
+    public void gsonDeserializeBoolTrue() throws IOException {
+        String val = "true";
+        boolean exp = true;
+
+        boolean gsonRes = gson.fromJson(val, boolean.class);
+        assertEquals(exp, gsonRes);
+
+        boolean jacksonRes = jackson.readValue(val, boolean.class);
+        assertEquals(exp, jacksonRes);
+
+        boolean gensonRes = genson.deserialize(val, boolean.class);
+        assertEquals(exp, gensonRes);
+    }
 
     @Test
     public void gsonDeserNullPrimitive() {
@@ -55,13 +103,11 @@ public class Case4PrimitiveNulls {
 
 
     public static class ClassWithPrimitive {
-
         private int primitive;
 
         public int getPrimitive() {
             return primitive;
         }
-
         public void setPrimitive(int primitive) {
             this.primitive = primitive;
         }
