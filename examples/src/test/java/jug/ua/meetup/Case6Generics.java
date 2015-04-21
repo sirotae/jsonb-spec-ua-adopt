3de5614b462f9884193693f4aeeb96ff5244ae37
Olena_Syrota <sirotae@gmail.com>
2015-04-21 20:22:34 +0300

    gson generic example, test code refactoring

--- examples/src/test/java/jug/ua/meetup/Case6Generics.java
+++ examples/src/test/java/jug/ua/meetup/Case6Generics.java
@@ -1,14 +1,17 @@
 package jug.ua.meetup;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.owlike.genson.GenericType;
 import com.owlike.genson.Genson;
 import com.owlike.genson.GensonBuilder;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,19 +25,14 @@ import static org.junit.Assert.assertEquals;
  */
 public class Case6Generics {
 
-    private Gson gson;
+    private Gson gson = new Gson();
 
     private ObjectMapper jackson = new ObjectMapper();
 
-    private Genson genson = new GensonBuilder().useRuntimeType(true).create();
-
-
-    public void gsonDeserToGeneric() {
-
-    }
+    private Genson genson = new Genson();
 
     @Test
-    public void jacksonDeserToGeneric() throws IOException {
+    public void gsonDeserToGeneric() throws IOException {
         String jsonString = "[{\n" +
                 "  \"title\": \"Super Book1\",\n" +
                 "  \"author\": \"Super Author1\"\n" +
@@ -43,36 +41,22 @@ public class Case6Generics {
                 "  \"title\": \"Super Book2\",\n" +
                 "  \"author\": \"Super Author2\"\n" +
                 "}]";
+        List<Book> exp = new ArrayList<Book>();
+        exp.add(new Book("Super Book1", "Super Author1"));
+        exp.add(new Book("Super Book2", "Super Author2"));
 
+
+        Type fooType = new TypeToken<List<Book>>(){}.getType();
+        List<Book> gsonRes = gson.fromJson(jsonString, fooType);
+        assertEquals(exp, gsonRes);
 
         JavaType collectionType = jackson.getTypeFactory().constructCollectionType(List.class, Book.class);
-        List<Book> readValue = jackson.readValue(jsonString, collectionType);
+        List<Book> jacksonRes = jackson.readValue(jsonString, collectionType);
+        assertEquals(exp, jacksonRes);
 
-        List<Book> exp = new ArrayList<Book>();
-        exp.add(new Book("Super Book1", "Super Author1"));
-        exp.add(new Book("Super Book2", "Super Author2"));
+        List<Book> gensonRes = genson.deserialize(jsonString, new GenericType<List<Book>>(){});
+        assertEquals(exp, gensonRes);
 
-        assertEquals(exp, readValue);
-    }
-
-    @Test
-    public void gensonDeserToGeneric() throws IOException {
-        String jsonString = "[{\n" +
-                "  \"title\": \"Super Book1\",\n" +
-                "  \"author\": \"Super Author1\"\n" +
-                "},\n" +
-                "{" +
-                "  \"title\": \"Super Book2\",\n" +
-                "  \"author\": \"Super Author2\"\n" +
-                "}]";
-
-        List<Book> readValue = genson.deserialize(jsonString, new GenericType<List<Book>>(){});
-
-        List<Book> exp = new ArrayList<Book>();
-        exp.add(new Book("Super Book1", "Super Author1"));
-        exp.add(new Book("Super Book2", "Super Author2"));
-
-        assertEquals(exp, readValue);
     }
 
 }
